5aa55cf455e154d7d239019b7d63960c77c8b58a
CadeWeiskopf <cweiskopf@thisiscsg.com>
2023-09-16 22:03:21 -0400

    stuff

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct, products } from '../data/data';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent {
+  product!: IProduct;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const slug = params.get('slug');
+      const productDetails = products.find((product) => product.slug === slug);
+      if (!productDetails) {
+        throw Error('could not find product by slug');
+      }
+      console.log(`product details`, productDetails);
+      this.product = productDetails;
+    });
+  }
 }
