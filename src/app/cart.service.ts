5e7de481b4aa3c2271efcccd8b391f866b249163
CadeWeiskopf <cweiskopf@thisiscsg.com>
2023-09-16 22:49:50 -0400

    ICartProduct

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -2,19 +2,24 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { IProduct } from './data/data';
 
+interface ICartProduct {
+  product: IProduct;
+  quantity: number;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  private cartSubject = new BehaviorSubject<any[]>([]);
+  private cartSubject = new BehaviorSubject<ICartProduct[]>([]);
 
   cart$ = this.cartSubject.asObservable();
 
-  updateCart(newItems: { product: IProduct; quantity: number }[]): void {
+  updateCart(newItems: ICartProduct[]): void {
     const currentCart = this.cartSubject.value;
     const updatedCart = [...currentCart, ...newItems];
 
-    // TODO: handle if product is in cart already
+    // TODO: handle if product is in cart already?
 
     this.cartSubject.next(updatedCart);
   }
